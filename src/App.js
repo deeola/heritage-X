0a7812d9da04d7f57455a25fd1038a44f76c0c0e
Adeola Bamigboye <clazikar@gmail.com>
2021-08-19 10:20:34 +0200

    add provider and store

--- src/App.js
+++ src/App.js
@@ -1,67 +1,54 @@
-import '../src/css/style.css';
-import Home from '../src/components/home/Home';
-import React,{useEffect} from 'react';
-import HeritageState from './components/context/Heritage/HeritageState';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Country from './components/home/Country';
-import Region from './components/home/Region';
-import Category from './components/home/Category';
-import Site from './components/home/Site';
-import Afro from './components/home/Afro';
-import Euro from './components/home/Euro';
-import Latino from './components/home/Latino';
-import Asias from './components/home/Asias';
-import Arabo from './components/home/Arabo';
-import Visited from './components/visited/Visited';
-import Bucketlist from './components/bucketlist/Bucketlist';
-import SignUp from './components/login/SignUp';
-import Login from './components/login/Login';
-import Layer from './components/shared/Layer';
+import "../src/css/style.css";
+import Home from "../src/components/home/Home";
+import React, { useEffect } from "react";
+import HeritageState from "./components/context/Heritage/HeritageState";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Country from "./components/home/Country";
+import Region from "./components/home/Region";
+import Category from "./components/home/Category";
+import Site from "./components/home/Site";
+import Afro from "./components/home/Afro";
+import Euro from "./components/home/Euro";
+import Latino from "./components/home/Latino";
+import Asias from "./components/home/Asias";
+import Arabo from "./components/home/Arabo";
+import Visited from "./components/visited/Visited";
+import Bucketlist from "./components/bucketlist/Bucketlist";
+import SignUp from "./components/login/SignUp";
+import Login from "./components/login/Login";
+import Layer from "./components/shared/Layer";
 
+import { Provider } from "react-redux";
+import store from "./store";
 
-
-function App() {
-
-
-
-
-
+const App = () => {
   return (
-    <HeritageState>
-      <Router>
-        
-      <div className="App">
-      <Switch>
-        <Route exact path="/" render={(props) => <Home /> } /> 
-        <Route  path="/countries" exact component={Country} /> 
-        <Route path="/regions" exact component={Region}  /> 
-        <Route  path="/categories" exact component={Category} /> 
-        <Route path='/Afro' exact component={Afro} />
-        <Route path='/Euro' exact component={Euro} />
-        <Route path='/Latino' exact component={Latino} />
-        <Route path='/Asias' exact component={Asias} />
-        <Route path='/Arabo' exact component={Arabo} />
-        <Route path='/Visited'  component={Visited} />
-        <Route path='/Bucketlist'  component={Bucketlist} />
-        <Route path='/SignUp'  component={SignUp} />
-        <Route path='/Login'  component={Login} />
-        <Route path='/:id'  component={Site} />
-        
-
-                  
-      </Switch>
-      <Layer />
-
-      </div>
-      </Router>
-      
-    </HeritageState>
-    
+    <Provider store={store}>
+      <HeritageState>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" render={(props) => <Home />} />
+              <Route path="/countries" exact component={Country} />
+              <Route path="/regions" exact component={Region} />
+              <Route path="/categories" exact component={Category} />
+              <Route path="/Afro" exact component={Afro} />
+              <Route path="/Euro" exact component={Euro} />
+              <Route path="/Latino" exact component={Latino} />
+              <Route path="/Asias" exact component={Asias} />
+              <Route path="/Arabo" exact component={Arabo} />
+              <Route path="/Visited" component={Visited} />
+              <Route path="/Bucketlist" component={Bucketlist} />
+              <Route path="/SignUp" component={SignUp} />
+              <Route path="/Login" component={Login} />
+              <Route path="/:id" component={Site} />
+            </Switch>
+            <Layer />
+          </div>
+        </Router>
+      </HeritageState>
+    </Provider>
   );
-}
-
+};
 
 export default App;
-
-
-
--- src/index.js
+++ src/index.js
@@ -3,24 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-import allReducers from "./reducers";
-import { Provider } from "react-redux";
-import { myCountries, Continent } from "./actions";
-
-let store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(Continent("Africa"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
